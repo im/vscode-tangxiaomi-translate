bc4fd83ccf1d1eaddde11a5708a6cda40b75ae31
糖小米 <tangxiaomiemail@gmail.com>
2019-08-31 10:19:44 +0800

    update

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,13 @@
 const humanizeString = require('humanize-string');
 const { youdao, baidu, google } = require('translation-api');
-// const { env, Position, window } = require('vscode');
+const getLink = require('../utils/link');
+const { translateLink } = require('../utils/github');
 
 function Translate (documents, connection) {
     this._connection = connection
     this._documents = documents
 }
+
 
 Translate.prototype.getText = async function (textDocumentPosition) {
     let block = await this.getSelectionContainPosition(textDocumentPosition);
@@ -18,21 +20,13 @@ Translate.prototype.getText = async function (textDocumentPosition) {
         } catch(e) {
             console.log(e)
         }
-        // global.translateProxyUrl = 'http://www.baidu.com'
-        // console.log(env)
-        // 
-        // [![alt text](http://path/to/img.jpg "title")](你的链接地址)
-        
-        // const str = '![](https://raw.githubusercontent.com/im/vscode-tangxiaomi-translate/master/images/google.jpg){:height="20px" width="20px"}'
-        const str = '[![test image size](https://raw.githubusercontent.com/im/vscode-tangxiaomi-translate/master/images/google.png)](https://raw.githubusercontent.com/im/vscode-tangxiaomi-translate/master/images/google.jpg)'
-        if (targetLanguageComment) {
-            contents.push(`${str} [Tangxiaomi Translate]    [Google](${targetLanguageComment.link})`)
-            contents.push(`${block.comment} =>`)
-            contents.push(targetLanguageComment.result)
+        if (!targetLanguageComment) {
+            contents.push(`${translateLink('google', targetLanguageComment.link)}`)
+            contents.push(`${block.comment} => ${targetLanguageComment.result}`)
             contents.push(' ')
         } else {
-            contents.push(`[Tangxiaomi Translate]`)
-            contents.push('翻译出错啦！~~~')
+            contents.push(`${translateLink('google', getLink('baidu', humanize))}`)
+            contents.push('翻译失败啦！~~~')
         }
         return {
             contents: contents,
